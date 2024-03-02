aad0425144f83f100fe64e5b22465090006a223d
wei <x981235xyvw@gmail.com>
2024-03-03 05:05:55 +0800

    Implement q_remove_head

--- queue.c
+++ queue.c
@@ -70,7 +70,19 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *first_entry = list_first_entry(head, element_t, list);
+
+    list_del(&first_entry->list);
+
+    if (sp) {
+        strncpy(sp, first_entry->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return first_entry;
 }
 
 /* Remove an element from tail of queue */
